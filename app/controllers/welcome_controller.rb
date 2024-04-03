bdfdfe08794621fa071ea73000e8ffd42af0699c
Odell McFarland IV <odell.mac4@gmail.com>
2024-04-02 22:25:14 -0700

    Attempt authorization 6

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     @users = User.all
+
+    # if session[:user_id].nil?
+    #   flash[:notice] = "You must be logged in or registered to access a user's dashboard."
+    #   redirect_to root_path
+    # end
   end
 end
